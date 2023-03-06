64398bb772c0d2662cbf3751cf5bec6ad1cdfaf2
outdead <outdead@mail.ru>
2023-03-06 20:21:57 +0300

    Add license notes to logfinder

---
+++ plugins/logsfind.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Statistics plugin for Project Zomboid Linux Server Manager.
+#
+# Copyright (c) 2023 Pavel Korotkiy (outdead).
+# Use of this source code is governed by the MIT license.
